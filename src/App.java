9cdd37f2c8512a13208f3ef15a5cdd2a10c2f69b
Demetrius <demetrius.mp789@gmail.com>
2021-07-08 20:44:08 -0400

    Entidade departamento

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Connection;
-
-import db.DB;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Connection c = DB.getConnection();
-    }
-}
